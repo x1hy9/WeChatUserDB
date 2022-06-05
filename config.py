5ad6b7c0598ab09c7c7b085aca62e4cce07781a0
x1hy9 <2607295815@qq.com>
2022-06-05 23:22:01 +0800

    v0.0.1

---
+++ config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+@ Author: HeliantHuS
+@ Codes are far away from bugs with the animal protecting
+@ Time:  6/23/2020
+@ FileName: config.py
+"""
+
+# wechatVersion = "2.6.6.28"
+# wechatOffset = 0x1131B64
+
+wechatVersion = "2.9.0.123"
+wechatOffset = 0x16B4D50
+
+# wechatVersion = "2.9.0.112"
+# wechatOffset = 0x16B4C70
